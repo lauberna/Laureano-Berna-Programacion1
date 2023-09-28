196d982a9bcf3ef5ff916f2f48d7e194506a63df
lauberna <laureanoberna2002@gmail.com>
2023-09-28 13:30:04 -0300

    Ejercicios complementarios

---
+++ ej..py
@@ -0,0 +1,63 @@
+# 1
+numero1 = 2
+
+# 2
+numero2 = 2.4
+
+# 3
+suma = numero1 + numero2
+print("suma: ", suma)
+# 4
+resta = numero1 - numero2
+print("resta:", resta)
+division = numero1 / numero2
+print("división:", division)
+multiplicacion = numero1 * numero2
+print("Multiplicación", multiplicacion)
+
+# 5
+nombre = "laureano"
+
+# 6
+precio = 500.50
+
+# 7
+descuento = .15
+
+# 8
+precio_final = precio - (precio * descuento)
+print(f'El valor final del producto es {precio_final}')
+
+# 9
+cadena = "programacion"
+
+# 10
+longitud = len(cadena)
+print(f'La longitud de la cadena, {cadena}, es: {longitud}')
+
+# 11
+precio = int(20.5)
+
+# 12
+nombre = "Laureano"
+apellido = "Berna"
+nombre_completo = nombre + " " + apellido
+print(f'Nombre completo: {nombre_completo}')
+
+# 13
+edad = 20
+edad += 5
+edad -= 10
+print(f'Edad {edad}')
+
+# 14
+altura = 1.70
+
+# 15
+nombre_mayus = "LAUREANO"
+nombre_min = nombre_mayus.lower()
+print(f'Nombre en minusculas: {nombre_min}')
+
+# 16
+nombre_mayus = nombre_mayus.lower().capitalize()
+print(f'Nombre con primera letra mayuscula: {nombre_mayus}')
